f1146f033dc32cc5f617146122c301fcf6d63af1
sarapanes <pdhmkup@gmail.com>
2021-12-24 15:17:43 +0000

    Initial commit

---
+++ refactor/figuras/Circunferencia.java
@@ -0,0 +1,43 @@
+package figuras;
+
+public class Circunferencia {
+    private double radio;
+    private String color;
+
+    public Circunferencia(double radio) {
+        this.radio = radio;
+    }
+
+    public void imprimir() {
+        color = "rojo";
+        System.out.println("Di�metro: " + (2 * radio));
+        System.out.println("Color: " + color);
+        double area = 2 * 3.1416 * radio * radio;
+        System.out.println(area);
+    }
+
+    public double getRadio() {
+		return radio;
+	}
+
+	public void setRadio(double radio) {
+		this.radio = radio;
+	}
+
+	public boolean esIgual(Circunferencia otro, boolean conDecimales) {
+        double radio1 = this.radio;
+        double radio2 = otro.getRadio();
+        if (conDecimales) {
+            if (radio1 == radio2)
+                return true;
+            else
+                return false;
+        } else {
+            if (Math.abs(radio1 - radio2) < 1)
+                return true;
+            else
+                return false;
+        }
+    }
+}
+
---
+++ refactor/figuras/Test.java
@@ -0,0 +1,16 @@
+package figuras;
+
+public class Test {
+    public static void main(String[] args) {
+        Circunferencia c1 = new Circunferencia(5.5);
+        Circunferencia c2 = new Circunferencia(10.1);
+        Circunferencia c3 = new Circunferencia(10.9);
+
+        if (c2.esIgual(c3, false))
+            System.out.println("c2 y c3: iguales sin considerar decimales");
+
+        if (c2.esIgual(c3, true))
+            System.out.println("c2 y c3: iguales considerando decimales");
+        System.out.println("Práctica realizada por Sara Panés de Hita");
+    }
+}
